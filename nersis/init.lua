c01f69fd1e45464587a847b0e5318f687c732ac6
Dwayne Slater <ds84182@gmail.com>
2015-05-02 19:58:00 -0400

    Make libfucklimitations more useful by allowing us to load libraries globally. Untested on both Windows and Mac OS X, but they should be supported now.

--- nersis/init.lua
+++ nersis/init.lua
@@ -1,8 +1,11 @@
 local nersis = {}
 nersis.VERSION = "Applebutter v1"
 
+local sharedEnding = ({Windows="dll",["OS X"]="dylib"})[love.system.getOS()] or "so"
+
 function nersis.init()
-	require "nersis.fucklimitations" --loads nersis.core with RTLD_GLOBAL
+	nersis.dlopen = require "nersis.fucklimitations" --loads nersis.core with RTLD_GLOBAL, and then gives us a dlopener
+	nersis.dlopen("./nersis/core."..sharedEnding)
 	nersis.core = require "nersis.core" --require the core library shared object
 	nersis.core.hello()
 	
@@ -15,6 +18,7 @@ function nersis.init()
 end
 
 function nersis.loadModule(name)
+	nersis.dlopen("./nersis/"..name.."."..sharedEnding)
 	require("nersis."..name)
 end
 
--- nersis/libfucklimitations/dummy.cpp
+++ nersis/libfucklimitations/dummy.cpp
@@ -1,7 +1,48 @@
 #include "core.h"
+
+#if defined(_WIN32)
+
+#include <windows.h>
+
+static void openGlobal(const char *lib) {
+	LoadLibraryA(lib);
+}
+
+#elif defined(__APPLE__)
+
+#include <mach-o/dyld.h>
+
+static void openGlobal(const char *lib) {
+	NSObjectFileImage img;
+	NSObjectFileImageReturnCode ret;
+	/* this would be a rare case, but prevents crashing if it happens */
+	if(!_dyld_present()) {
+		return;
+	}
+	ret = NSCreateObjectFileImageFromFile(path, &img);
+	if (ret == NSObjectFileImageSuccess) {
+		NSModule mod = NSLinkModule(img, path, NSLINKMODULE_OPTION_RETURN_ON_ERROR);
+		NSDestroyObjectFileImage(img);
+	}
+}
+
+#else
+
 #include <dlfcn.h>
 
-LUALIB_API int luaopen_nersis_fucklimitations(lua_State *L) {
-	dlopen("./nersis/core.so",RTLD_GLOBAL|RTLD_NOW);
+static void openGlobal(const char *lib) {
+	dlopen(lib,RTLD_GLOBAL|RTLD_NOW);
+}
+
+#endif
+
+static int nersis_fl_dlopen(lua_State *L) {
+	const char *file = luaL_checkstring(L, 1);
+	openGlobal(file);
 	return 0;
+}
+
+LUALIB_API int luaopen_nersis_fucklimitations(lua_State *L) {
+	lua_pushcfunction(L, nersis_fl_dlopen);
+	return 1;
 }
